cc28f8ef6c5b5e19791cd20c5313e442eddefacb
User <anvarrashidov17@gmail.com>
2023-07-18 13:15:37 +0500

    Added roles

--- controllers/refreshController.js
+++ controllers/refreshController.js
@@ -1,32 +1,38 @@
 const usersDB = {
-    users : require('../model/users.json'),
-    setUsers : function(data){ this.users = data }
-}   
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
 const jwt = require("jsonwebtoken")
 require('dotenv').config();
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies
-    if(!cookies?.jwt) return res.sendStatus (401)
+    if (!cookies?.jwt) return res.sendStatus(401)
     console.log(cookies.jwt);
     const refreshToken = cookies.jwt;
 
-    const foundUser =  usersDB.users.find(person => person.refreshToken === refreshToken) 
-    if(!foundUser) return res.status(401) //Forbidden 
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken)
+    if (!foundUser) return res.status(401) //Forbidden 
     // evaluate jwt
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            if(err || foundUser.username !== decoded.username) return res.sendStatus(403)
+            if (err || foundUser.username !== decoded.username) return res.sendStatus(403)
+            const roles = Object.values(foundUser.roles);
             const accesToken = jwt.sign(
-                {"username" : decoded.username},
+                {
+                    "UserInfo": {
+                        "username": decoded.username,
+                        "roles": roles
+                    }
+                },
                 process.env.ACCESS_TOKEN_SECRET,
-                {expiresIn: '30s'}
+                { expiresIn: '30s' }
             );
             res.json({ accesToken })
         }
-    ) 
+    )
 }
 
-module.exports = {handleRefreshToken}
+module.exports = { handleRefreshToken }
--- routes/api/employees.js
+++ routes/api/employees.js
@@ -2,13 +2,14 @@ const express = require("express")
 const router = express.Router()
 const employeesController = require("../../controllers/employeesController")
 const verifyJWT = require("../../middleware/verifyJWT")
+const ROLES_LIST = require('../../config/rolesList')
+const verifyRoles = require('../../middleware/verifyRoles')
 
 router.route('/')
       .get(employeesController.getAllEmployees)
       .post(employeesController.createNewEmployee)
       .put(employeesController.updateEmployee)
       .delete(employeesController.deleteEmployee);
-
 router.route("/:id")
       .get(employeesController.getEmployee)
 module.exports = router
